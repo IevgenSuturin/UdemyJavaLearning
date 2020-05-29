ad6ac442cf09cbf5b156989fb60c6fda870eeebf
Yevhen Suturin <esuturin77@gmail.com>
2020-05-29 14:15:49 +0300

    Start learning NIO routine. Discuss Path, Files, BufferedReader, BufferedWriter classes. NIO serialization approach. Start discussing ByteBuffer, FileChannel. Files.readAllLines() method is very convenient.

--- InputOutput/src/com/yevhensuturin/Locations.java
+++ InputOutput/src/com/yevhensuturin/Locations.java
@@ -7,12 +7,14 @@ public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new LinkedHashMap<>();
 
     public static void main(String[] args) throws IOException {
-        try (ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")))){
+//Serialization
+        try (ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("locations_big.dat")))){
             for(Location location: locations.values()){
                 locFile.writeObject(location);
             }
         }
 
+// Binary input/output
 //        try (DataOutputStream locFile = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")))){
 //            for(Location location: locations.values()){
 //                locFile.writeInt(location.getLocationID());
@@ -30,6 +32,7 @@ public class Locations implements Map<Integer, Location> {
 //            }
 //        }
 
+// Character input/output
 //        try (BufferedWriter localFile = new BufferedWriter(new FileWriter("locations.txt"));
 //             BufferedWriter dirFile = new BufferedWriter(new FileWriter("directions.txt")) ){
 //            for(Location location: locations.values()){
@@ -43,20 +46,29 @@ public class Locations implements Map<Integer, Location> {
 //        }
     }
 
+
+
+    //1. First four bytes will contain the number of locations (bytes 0-3)
+    //2. The next four bytes will contain the start offset of the locations section (bytes 4-7)
+    //3. The next section of the file will contain the index (the index is 169 bytes long. It will start at byte 8 and end at byte 1699
+    //4. The final section of the file will contain the location records (the data). It will start at byte 1700.
+
     static {
-        try(ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(new FileInputStream("locations.dat")))){
+        try(ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(new FileInputStream("locations.dat")))) {
             boolean eof = false;
-            while (!eof){
+            while (!eof) {
                 try {
                     Location location = (Location) locFile.readObject();
                     System.out.println("Read location " + location.getLocationID() + ": " + location.getDescription());
                     System.out.println("Found " + location.getExits().size() + " exits");
 
                     locations.put(location.getLocationID(), location);
-                }catch (EOFException e){
+                } catch (EOFException e) {
                     eof = true;
                 }
             }
+        }catch(InvalidClassException e){
+            System.out.println("InvalidClassException "+e.getMessage());
         } catch (IOException io){
             System.out.println("IO Exception" + io.getMessage());
         } catch (ClassNotFoundException e){
