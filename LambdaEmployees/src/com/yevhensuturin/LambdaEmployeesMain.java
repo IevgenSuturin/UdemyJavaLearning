29792e1f032e722819b825733e4dbedb7b2216e3
Yevhen Suturin <esuturin77@gmail.com>
2020-06-15 15:55:41 +0300

    Finish Lambda theoretical material

--- LambdaEmployees/src/com/yevhensuturin/LambdaEmployeesMain.java
+++ LambdaEmployees/src/com/yevhensuturin/LambdaEmployeesMain.java
@@ -2,7 +2,8 @@ package com.yevhensuturin;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Random;
+import java.util.function.*;
 
 public class LambdaEmployeesMain {
     public static void main(String[] args) {
@@ -21,6 +22,42 @@ public class LambdaEmployeesMain {
         employees.add(red);
         employees.add(charming);
 
+        Function<Employee, String> getLastName = (employee) -> {return employee.getName().substring(employee.getName().indexOf(" ")+1);};
+        String lastName = getLastName.apply(employees.get(1));
+        System.out.println(lastName);
+        Function<Employee, String> getFirstName = (employee) -> {return employee.getName().substring(0, employee.getName().indexOf(" "));};
+
+        System.out.println(getName(getFirstName, employees.get(1)));
+        System.out.println(getName(getLastName, employees.get(1)));
+
+        Random random1 = new Random();
+        for (Employee employee: employees){
+            if (random1.nextBoolean()){
+                System.out.println(getName(getFirstName, employee));
+            } else {
+                System.out.println(getName(getFirstName, employee));
+            }
+        }
+
+        Function<Employee, String> upperCase = employee -> employee.getName().toUpperCase();
+        Function<String, String> firstName = name -> name.substring(0, name.indexOf(" "));
+        Function chainedFunction = upperCase.andThen(firstName);
+        System.out.println(chainedFunction.apply(employees.get(0)));
+
+        BiFunction<String, Employee, String> concatAge = (String name, Employee employee)->{
+            return name.concat(" " + employee.getAge());
+        };
+
+        String upperName = upperCase.apply(employees.get(0));
+        System.out.println(concatAge.apply(upperName, employees.get(0)));
+
+        IntUnaryOperator incBy5 = i -> i + 5;
+        System.out.println(incBy5.applyAsInt(10));
+
+        Consumer<String> c1 = s -> s.toUpperCase();
+        Consumer<String> c2 = s -> System.out.println(s);
+        c1.andThen(c2).accept("Hello world!");
+
 //        System.out.println("Employees over 30.");
 //        employees.forEach(employee -> {if(employee.getAge()>30) System.out.println(employee);});
 //
@@ -31,8 +68,39 @@ public class LambdaEmployeesMain {
 //            }
 //        });
 
-        printEmployeesByAge(employees, "Older than 30", employee -> employee.getAge()>30);
-        printEmployeesByAge(employees, "\nEmployees 30 and younger", employee -> employee.getAge()<=30);
+//        printEmployeesByAge(employees, "Older than 30", employee -> employee.getAge()>30);
+//        printEmployeesByAge(employees, "\nEmployees 30 and younger", employee -> employee.getAge()<=30);
+//        printEmployeesByAge(employees, "\nEnployees ynger than 25", new Predicate<Employee>() {
+//            @Override
+//            public boolean test(Employee employee) {
+//                return employee.getAge()<25;
+//            }
+//        });
+//
+//        IntPredicate greaterThan15 = i -> i > 15;
+//        IntPredicate lessThan100 = i -> i < 100;
+//
+//        System.out.println(greaterThan15.test(10));
+//        int a=20;
+//        System.out.println(greaterThan15.test(a+5));
+//
+//        System.out.println(greaterThan15.and(lessThan100).test(50));
+//        System.out.println(greaterThan15.and(lessThan100).test(15));
+//
+//        Random random = new Random();
+//        Supplier<Integer> randomSupplier = () -> random.nextInt(1000);
+//        for (int i=0; i<10; i++){
+//            System.out.println(randomSupplier.get());
+//        }
+//
+//        employees.forEach(employee -> {
+//            String lastName = employee.getName().substring(employee.getName().indexOf(" ")+1);
+//            System.out.println("Last name: "+lastName);
+//        });
+    }
+
+    private static String getName(Function<Employee, String> getName, Employee employee){
+        return getName.apply(employee);
     }
 
     private static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> ageCondition){
